aa9b45bd7a6c21888c8a4727965b420612d6d86f
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-11-23 15:22:32 +0530

    [Gyanendra] Add: A Program to Add Index Value in a Figure Format of Binary Tree

--- Collection/Collection/Program.cs
+++ Collection/Collection/Program.cs
@@ -11,6 +11,15 @@ namespace Collection
             binarySearch.Insert(30);
             binarySearch.Insert(70);
             binarySearch.Insert(72);
+            binarySearch.Insert(40);
+            binarySearch.Insert(60);
+            binarySearch.Insert(95);
+            binarySearch.Insert(11);
+            binarySearch.Insert(65);
+            binarySearch.Insert(31);
+            binarySearch.Insert(16);
+            binarySearch.Insert(63);
+            binarySearch.Insert(67);
             binarySearch.Display();
             binarySearch.GetSize();
         }
