666f94495d8cb755694907c2743770b99bfa9065
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-11-23 12:07:30 +0530

    [Gyanendra] Add: A Demo Program of Hash Table

---
+++ Collection/Collection/Hash Table/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hash Table Demo");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+            string hash5 = hash.Get("5");
+            Console.WriteLine("5th Index Value: " +hash5);
+        }
+    }
+}
