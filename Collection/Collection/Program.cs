eeb4a8e140b20c7168d3507898b34c438291f355
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-11-23 15:11:55 +0530

    [Gyanendra] Add: A Program to Add Index Value in Binary Tree

---
+++ Collection/Collection/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Binary Search Tree Demo");
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Insert(72);
+            binarySearch.Display();
+            binarySearch.GetSize();
+        }
+    }
+}
